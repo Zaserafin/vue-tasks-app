99648b24f62f610773c75d8d24c4fd5355fc54b7
Zaserafin <mauro.zaserafin@gmail.com>
2023-07-28 17:34:07 -0300

    fix: (Imports) Fixed TaskList import

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -1,18 +1,15 @@
 <script setup>
 import Panel from "@/components/Panel.vue";
 import TaskForm from "@/components/TaskForm.vue";
-import TaskList from "../components/TaskList.vue";
+import TaskList from "@/components/TaskList.vue";
 </script>
 
 <template>
   <div class="h-full p-2 max-w-3xl mx-auto gap-10 md:py-6 md:px-0">
     <Panel>
       <template #title>Lista de tareas</template>
-
       <TaskForm />
-
       <TaskList />
-
     </Panel>
   </div>
 </template>
