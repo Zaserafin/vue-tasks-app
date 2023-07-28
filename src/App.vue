3a30e9ba6d8f63fda459c7043d8e113840f5567d
Zaserafin <mauro.zaserafin@gmail.com>
2023-07-27 23:13:30 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <main class="h-full flex items-center justify-center">
+    <RouterView />
+  </main>
+</template>
+ 
---
+++ src/views/AppView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Application!</h1>
+</template>
