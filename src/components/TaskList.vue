9834f23ef40b91af9bee30c2fc590fb2068a3b64
Zaserafin <mauro.zaserafin@gmail.com>
2023-07-29 20:37:19 -0300

    feat: (task-list) added "no tasks" message

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -15,6 +15,7 @@ function handleDeleteTask(task) {
 
 <template>
     <ul class="overflow-y-scroll h-[calc(100%_-_120px)] divide-y divide-slate-600 mt-4">
+        <h2 v-if="tasks.length === 0" class="text-lg my-2 text-center">No hay tareas registradas.</h2>
         <Task v-for="task in tasks" :key="task.id" :task="task" @delete="handleDeleteTask" />
     </ul>
 </template>
