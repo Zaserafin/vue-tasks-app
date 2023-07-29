c1327c38202da0b8957a665b8b2c60978535f3a6
Zaserafin <mauro.zaserafin@gmail.com>
2023-07-29 20:48:16 -0300

    feat: (components) added Badge component

---
+++ src/components/Badge.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <button
+        class="text-xs py-0.5 px-2 rounded-2xl border border-slate-400 text-slate-400 hover:text-slate-300 hover:border-slate-300">
+        <slot />
+    </button>
+</template>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Task from "@/components/Task.vue";
+import Badge from "./Badge.vue";
 
 defineProps({
     tasks: Array
@@ -14,6 +15,11 @@ function handleDeleteTask(task) {
 </script>
 
 <template>
+    <div class="flex flex-wrap gap-1 mt-4">
+        <Badge class="text-indigo-500 border-indigo-500">Todas</Badge>
+        <Badge>Completadas</Badge>
+        <Badge>En progreso</Badge>
+    </div>
     <ul class="overflow-y-scroll h-[calc(100%_-_120px)] divide-y divide-slate-600 mt-4">
         <h2 v-if="tasks.length === 0" class="text-lg my-2 text-center">No hay tareas registradas.</h2>
         <Task v-for="task in tasks" :key="task.id" :task="task" @delete="handleDeleteTask" />
