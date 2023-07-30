49f4ba08c4acffb9ccd324ba0a51a795494e9b79
Zaserafin <mauro.zaserafin@gmail.com>
2023-07-29 21:34:21 -0300

    feat: (app-view) added task filters

--- src/views/AppView.vue
+++ src/views/AppView.vue
@@ -3,18 +3,53 @@ import { ref, computed } from "vue";
 import Panel from "@/components/Panel.vue";
 import TaskForm from "@/components/TaskForm.vue";
 import TaskList from "@/components/TaskList.vue";
+import Badge from "@/components/Badge.vue";
+
+const filters = [
+  {
+    id: "all",
+    label: "Todas",
+    key: "complete",
+    value: null,
+    default: true
+  },
+  {
+    id: "completed",
+    label: "Completadas",
+    key: "complete",
+    value: true,
+    default: false
+  },
+  {
+    id: "inProgress",
+    label: "En progreso",
+    key: "complete",
+    value: false,
+    default: false
+  },
+];
 
 const tasks = ref([
-  { id: 1, name: "Testing Task 1 Application", complete: false },
-  { id: 2, name: "Testing Task 2 Application", complete: false },
+  { id: 0, name: "Testing Task 1 Application", complete: false },
+  { id: 1, name: "Testing Task 2 Application", complete: false },
 ]);
+
+const currentFilter = ref("all");
 
 const completedTasks = computed(() => tasks.value.filter(task => task.complete));
 const inProgressTasks = computed(() => tasks.value.filter(task => !task.complete));
 
+const filteredTask = computed(() => {
+  const filter = filters.find(f => f.id === currentFilter.value);
+  if (filter.id === "all") return tasks.value;
+  return tasks.value.filter(task => {
+    return task[filter.key] === filter.value;
+  });
+})
+
 function handleAddTask(newTask) {
   const task = {
-    id: tasks.value.length + 2,
+    id: tasks.value.length + 1,
     name: newTask,
     complete: false
   };
@@ -44,7 +79,13 @@ function handleDeleteTask(id) {
         </div>
       </template>
       <TaskForm @submit="handleAddTask" />
-      <TaskList :tasks="tasks" @delete="handleDeleteTask" />
+
+      <div class="flex flex-wrap gap-1 mt-4">
+        <Badge v-for="filter in filters" :class="{ 'text-indigo-500 border-indigo-500': filter.id === currentFilter }"
+          :key="filter.id" @click="currentFilter = filter.id">{{ filter.label }}</Badge>
+      </div>
+
+      <TaskList :tasks="filteredTask" @delete="handleDeleteTask" />
     </Panel>
   </div>
 </template>
