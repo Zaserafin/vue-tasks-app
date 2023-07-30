1770844504f0d4e88f6adc1c0db7d13f8361bedf
Zaserafin <mauro.zaserafin@gmail.com>
2023-07-30 01:31:24 -0300

    fix: (panel) fixed overflow bug

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="h-full p-4 rounded-md bg-slate-700 border-slate-600 text-white">
+    <div class="h-full flex flex-col p-4 rounded-md bg-slate-700 border-slate-600 text-white overflow-y-hidden">
         <div v-if="$slots.heading" class="flex flex-col items-start justify-between mb-2 md:flex-row md:items-center">
             <slot name="heading" />
         </div>
